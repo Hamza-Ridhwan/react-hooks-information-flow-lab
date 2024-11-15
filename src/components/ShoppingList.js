4a59edd145515966c3d003d67355a1bbda365037
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-11-15 16:53:16 +0300

    Completed lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -10,24 +10,28 @@ function ShoppingList({ items }) {
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
+
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemsToDisplay.length > 0 ? (
+          itemsToDisplay.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))
+        ) : (
+          <p>No items found for this category.</p>
+        )}
       </ul>
     </div>
   );
